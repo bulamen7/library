3406e5197fa0397d22677979a7ce92a99ef5da71
Arkadiusz <swiokloarkadiusz@gmail.com>
2020-04-09 20:16:37 +0200

    added getters and setters

--- src/src/Book.java
+++ src/src/Book.java
@@ -19,4 +19,52 @@ public class Book {
         String info = title + " ;" + author + " ;" + releaseDate + " ;" + pages + " ;" + publisher + " ;" + isbn;
         System.out.println(info);
     }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getAuthor() {
+        return author;
+    }
+    
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+    public int getReleaseDate() {
+        return releaseDate;
+    }
+    
+    public void setReleaseDate(int releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+    
+    public int getPages() {
+        return pages;
+    }
+    
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+    
+    public String getPublisher() {
+        return publisher;
+    }
+    
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+    
+    public String getIsbn() {
+        return isbn;
+    }
+    
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 }
