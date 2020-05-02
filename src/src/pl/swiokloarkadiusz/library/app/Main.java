86060879038903925cd36c24de8a23841526b857
Arkadiusz Świokło <swiokloarkadiusz@gmail.com>
2020-05-02 10:17:59 +0200

    implement map and fix issues

--- src/src/pl/swiokloarkadiusz/library/app/Main.java
+++
@@ -1,8 +0,0 @@
-package pl.swiokloarkadiusz.library.app;
-
-public class Main {
-    public static void main(String[] args) throws NoSuchFieldException {
-        LibraryControl libraryControl = new LibraryControl();
-        libraryControl.controlLoop();
-    }
-}
